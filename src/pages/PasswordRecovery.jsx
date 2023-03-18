b4ab32ddafa26ec10fcc0a9d13a9ab29416c61bf
Evgenii <emeleshkov@mail.ru>
2023-03-18 14:45:02 -0300

    Fix action in `PasswordRecovery.jsx`

--- src/pages/PasswordRecovery.jsx
+++ src/pages/PasswordRecovery.jsx
@@ -3,7 +3,7 @@ import Form from '@/components/Form';
 import Input from '@/components/Input';
 
 export default 
-  <Form action="login" >
+  <Form action="passRecovery" >
     <h1>восстановление пароля</h1>
     <Input type="text" name="email" />
     <Button>Отправить ссылку на почту</Button>
