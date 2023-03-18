961c35398c841e37bd6154365f2002ecbb1a8a73
J.D <jd@phon.one>
2023-03-18 10:44:09 -0300

    Add common `<Form>` component

---
+++ src/components/Form.jsx
@@ -0,0 +1,21 @@
+import api from '@/lib/api'
+import parse from '@/lib/form2json'
+ 
+export default function Form (props) {
+  async function onSubmit (evt) {
+    evt.preventDefault()
+    const payload = parse(evt.target)
+    try {
+      const res = await api[props.action](payload)
+      props.callback(res)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      {props.children}
+    </form>
+  )
+}
