22c601d5f17d1c83c58e5fb05a8dff33e7a11ab9
J.D <jd@phon.one>
2023-03-18 10:46:52 -0300

    Add `@` alias to `src`

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Setup proxy to backend
     proxy: {
       '/api': {
         target: 'http://localhost:9000/',
@@ -12,5 +14,12 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
+  },
+  resolve: {
+    // To use @/componets, etc.
+    alias: [{ 
+      find: '@', 
+      replacement: resolve(__dirname, "src")
+    }]
   }
 })
