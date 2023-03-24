88acc4921f54122f1d60b14be0e8b2b8fbb32400
J.D <jd@phon.one>
2023-03-23 22:40:10 -0300

    Refactor `Login` page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom'
 
 export default
   <div className='auth-form'>
-    <Form action='login' callback={auth.set(localStorage)}>
+    <Form action='login' callback={auth.set(localStorage)} redirect="/">
       <h1>
         Войти &nbsp;
         <NavLink to='/register'>Регистрация</NavLink>
