48abb5716e705f4bee8a44e0cc46ef204b67fca5
J.D <jd@phon.one>
2023-03-23 22:39:54 -0300

    Refactor `Profile` page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,28 +4,35 @@ import API from '@/lib/api'
 import Image from '@/components/Image'
 
 export default function Profile () {
-  if (!localStorage.auth) return <Navigate to='/login' />  
-   
   const [user, setUser] = useState({})
-  useEffect(()=>{
-    const res = API.me()
-      .then(res=>setUser(res))
-  },[])  
-  console.log(user) 
-  const userStatus = (!user.expert && !user.admin) ? 'участник' : (user.expert && !user.admin) ? 'эксперт' : user.admin ? 'админ' : ''
-    
+  
+  useEffect(() => {
+    API.me().then(setUser).catch(
+      () => {
+        localStorage.removeItem('auth')
+        location.replace('/login')
+      }
+    )
+  }, [])
+
   return (
-  <div className='profile-style'>
-    <h1>Странца пользователя</h1>
-    <div className='one-x-three-greed'>
-      <div className='div1-1x3'>
-        <Image editable className='avatar' src={user.avatar} alt={user.firstName} />
+    <div className='profile-style'>
+      <h1>Страница пользователя</h1>
+      <div className='one-x-three-greed'>
+        <div className='div1-1x3'>
+          <Image 
+            editable 
+            className='avatar' 
+            src={user.avatar} 
+            alt={user.firstName} 
+          />
+        </div>
+        <div className='div2-1x3'>
+          <h2>{user.firstName} &nbsp; {user.lastName}</h2>
+        </div>
       </div>
-      <div className='div2-1x3'>
-        <p>{userStatus}</p>
-        <h2>{user.firstName} &nbsp; {user.lastName}</h2>
-      </div>      
     </div>
-  </div>
   )
 }
+
+
